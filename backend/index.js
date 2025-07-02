c70b4d3c67d380da179b103696541940fee260b3
Sanket Patil <sanketpatil574918@gmail.com>
2025-07-02 16:19:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -41,6 +41,13 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://pic-pe-ui.vercel.app'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); 
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+  next();
+});
+
 app.use("/auth", AuthRouter);
 app.use("/post", PostRouter);
 app.use("/messages", MessageRouter);
