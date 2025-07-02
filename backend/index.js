3799903e0d0e9ab0d8ee5584249fba86bedcb0f6
Sanket Patil <sanketpatil574918@gmail.com>
2025-07-02 16:13:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,12 +23,23 @@ main()
   });
 
 app.use(bodyParser.json());
-app.use(cors({
-  origin: "*", 
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      callback(null, origin); 
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.options("*", cors());
+
+app.use((req, res, next) => {
+  console.log("Request Origin:", req.headers.origin);
+  console.log("Request Method:", req.method);
+  next();
+});
 
 app.use("/auth", AuthRouter);
 app.use("/post", PostRouter);
