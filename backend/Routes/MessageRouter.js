5f2fd9146651553541d61417a6d0b86f972731aa
Sanket Patil <sanketpatil574918@gmail.com>
2025-09-02 21:01:29 +0530

    Update MessageRouter.js

--- backend/Routes/MessageRouter.js
+++ backend/Routes/MessageRouter.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const { sendMessage, getMessages, deleteMessage } = require("../Controllers/MessageController.js");
 const authenticateToken = require("../middleware/Middleware.js");
 
-router.post("/send", authenticateToken, sendMessage);
-router.get("/with/:otherId", authenticateToken, getMessages);
-router.delete("/:messageId", authenticateToken, deleteMessage);
+router.post("/send", sendMessage);
+router.get("/with/:otherId", getMessages);
+router.delete("/:messageId", deleteMessage);
 
 module.exports = router;
